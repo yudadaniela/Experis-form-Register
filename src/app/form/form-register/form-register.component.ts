07278ff7c69ba4e82b9f29910b1750a640e45bc0
Daniela <yudadaniela@hotmail.com>
2024-01-11 14:34:13 -0500

    validator asyn

--- src/app/form/form-register/form-register.component.ts
+++ src/app/form/form-register/form-register.component.ts
@@ -1,64 +1,95 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+  AbstractControl,
+  AsyncValidatorFn,
+  ValidationErrors,
+} from '@angular/forms';
 import { ServicesFormRegisterService } from 'src/app/services/services-form-register.service';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable, of } from 'rxjs';
+import { Register } from '../../models/register';
+import { map, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-form-register',
   templateUrl: './form-register.component.html',
   styleUrls: ['./form-register.component.css'],
 })
-export class FormRegisterComponent  {
+export class FormRegisterComponent {
   formRegister: FormGroup;
   constructor(
     private fb: FormBuilder,
     private formRegisterService: ServicesFormRegisterService,
-    private snackBar:MatSnackBar
+    private snackBar: MatSnackBar
   ) {
     this.formRegister = this.fb.group({
-      nickName: ['', [Validators.required, Validators.minLength(4)]],
-      firtName: ['', [Validators.required, Validators.minLength(4)]],
-      secondName: ['', Validators.minLength(4)],
-      firtSurtname: ['', [Validators.required, Validators.minLength(4)]],
-      secondSurtname: ['', Validators.minLength(4)],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+      personalInfo: this.fb.group({
+        nickName: ['', [Validators.required, Validators.minLength(4)]],
+        firtName: ['', [Validators.required, Validators.minLength(4)]],
+        secondName: ['', Validators.minLength(4)],
+        firtSurtname: ['', [Validators.required, Validators.minLength(4)]],
+        secondSurtname: ['', Validators.minLength(4)],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+      }),
+      addressInfo: this.fb.group({
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+        zipCode: ['', Validators.pattern(/^\d{5}$/)],
+      }),
     });
   }
 
-  showMessage(message:string, action:string){
-  this.snackBar.open(message, action,{
-    horizontalPosition:'center',
+ 
+  validateEmailAsync(
+    control: AbstractControl
+  ): Observable<ValidationErrors | null> {
+    const email = control.value;
+    console.log(email);
+    return of(this.formRegisterService.isEmailUnique(email)).pipe(
+      map((available) => (available ? null : { uniqueEmail: true }))
+    );
+  }
+  showMessage(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      horizontalPosition: 'center',
       verticalPosition: 'top',
       duration: 3000,
-  })
+    });
   }
+
   onsubmit() {
     console.log(this.formRegister.value);
-    
+
     if (this.formRegister.valid) {
       if (
         this.formRegisterService.isEmailUnique(
-          this.formRegister.get('email')?.value
+          this.formRegister.get('personalInfo.email')?.value
         )
       ) {
         const data = {
-          nickName: this.formRegister.get('nickName')?.value,
-          fullName: this.formRegister.get('firtName')?.value +' '+
-          this.formRegister.get('secondName')?.value +' '+
-          this.formRegister.get('firtSurtname')?.value +' '+
-          this.formRegister.get('secondSurtname')?.value,
-          email: this.formRegister.get('email')?.value,
-          password: this.formRegister.get('password')?.value,
+          nickName: this.formRegister.get('personalInfo.nickName')?.value,
+          fullName:
+            this.formRegister.get('personalInfo.firtName')?.value +
+            ' ' +
+            this.formRegister.get('personalInfo.secondName')?.value +
+            ' ' +
+            this.formRegister.get('personalInfo.firtSurtname')?.value +
+            ' ' +
+            this.formRegister.get('personalInfo.secondSurtname')?.value,
+          email: this.formRegister.get('personalInfo.email')?.value,
+          password: this.formRegister.get('personalInfo.password')?.value,
         };
         this.formRegisterService.addUser(data);
-        this.showMessage('Register','OK');
+        this.showMessage('Register', 'OK');
         console.log(this.formRegisterService.getUsers());
       } else {
-        this.showMessage('Register','The email is registered');
+        this.showMessage('Register', 'The email is registered');
       }
     }
-    
   }
-  
 }
